6bbfe7818ab3847c6d84fa5ca06c725c3695ff49
Yifan Lin <yflin001217@gmail.com>
2021-12-16 01:48:25 +0800

    fix some bugs

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from torch.utils.data import DataLoader, random_split
 from dataset import BiRdQA
 from torch.optim import Adam
 from torch.optim.lr_scheduler import ExponentialLR
-# from torch.utils.tensorboard import SummaryWriter
+from tensorboardX import SummaryWriter
 from models import ERNIEGuesser
+from tqdm import tqdm
 
 
 class Trainer:
@@ -14,8 +15,8 @@ class Trainer:
                  train_dataset,
                  test_dataset,
                  batch_size=64,
-                 n_epochs=200,
-                 lr=1e-4,
+                 n_epochs=400,
+                 lr=5e-5,
                  lr_decay=0.98,
                  print_every=10,
                  test_every=10,
@@ -37,13 +38,13 @@ class Trainer:
         self.optimizer = Adam((x for x in model.parameters() if x.requires_grad),
                               lr=lr)
         self.scheduler = ExponentialLR(self.optimizer, lr_decay)
-        # self.writer = SummaryWriter()
+        self.writer = SummaryWriter()
 
     def train(self):
         self.model.to(self.device)
         self.model.eval()
+        pbar = tqdm(total=self.n_epochs * len(self.train_dataloader.dataset))
         for epoch in range(self.n_epochs):
-            print(f"----------------Epoch {epoch}----------------")
             for batch in self.train_dataloader:
                 self.optimizer.zero_grad()
                 (riddle_input_ids,
@@ -59,7 +60,8 @@ class Trainer:
                 loss.backward()
                 self.optimizer.step()
                 self.step += 1
-                # self.writer.add_scalar('train/loss', loss.item(), self.step)
+                pbar.update(1)
+                self.writer.add_scalar('train/loss', loss.item(), self.step)
                 if self.step % self.print_every == 0:
                     print(f"Step {self.step}: loss = {loss.item()}")
             self.scheduler.step()
@@ -75,7 +77,6 @@ class Trainer:
         with torch.no_grad():
             all_loss = 0
             all_correct = 0
-            i_iter = 0
             for batch in self.test_dataloader:
                 (riddle_input_ids,
                  riddle_attention_mask,
@@ -91,11 +92,11 @@ class Trainer:
                 pred = torch.argmax(log_prob, dim=1)
                 correct = (pred == label).sum()
                 all_correct += correct
-                i_iter += 1
-            print(f'Test Loss = {all_loss / i_iter}')
-            print(f'Test Accuracy = {all_correct / len(self.test_dataloader)}')
-        # self.writer.add_scalar('test/loss', all_loss / i_iter, self.epoch)
-        # self.writer.add_scalar('test/accuracy', all_correct / len(self.test_dataloader), self.epoch)
+        loss = all_loss / len(self.test_dataloader)
+        accuracy = all_correct / len(self.test_dataloader.dataset)
+        self.writer.add_scalar('test/loss', loss, self.epoch)
+        self.writer.add_scalar('test/accuracy', accuracy, self.epoch)
+        print(f'Epoch {self.epoch}: test loss={loss}, test accuracy={accuracy}')
         self.model.train()
 
     def validate(self, dataset):
@@ -134,13 +135,17 @@ class Trainer:
 
 if __name__ == '__main__':
     device = torch.device(0)
+    print("Loading datasets...")
     train_dataset = BiRdQA('train.csv', 'wiki_info_v2.json', n_options=5, device=device)
     val_dataset = BiRdQA('val.csv', 'wiki_info_v2.json', n_options=5, device=device)
     train_size = int(len(train_dataset) * 0.8)
     test_size = len(train_dataset) - train_size
     train_dataset, test_dataset = random_split(train_dataset, [train_size, test_size])
 
+    print("Building model...")
     model = ERNIEGuesser(n_options=5, n_layers=6, n_hidden=512, feature_concat='all', use_ln=True, dropout=0.1)
     trainer = Trainer(model, train_dataset, test_dataset, device=device)
+
+    print("Start training")
     trainer.train()
     trainer.validate(val_dataset)
